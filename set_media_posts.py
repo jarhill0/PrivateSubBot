6700bfbafe24b33009907b87f794882aebf082d0
jarhill0 <jarhill0@users.noreply.github.com>
2019-12-08 11:48:33 -0500

    Add scheduling in Python since crontab can't

--- set_media_posts.py
+++ set_media_posts.py
@@ -1,22 +1,35 @@
+from datetime import date
 from sys import argv, exit, stderr
+
 import helpers
 
 try:
     import config
 except (ImportError, ModuleNotFoundError):
     print(
-        "config.py not found. Rename config.example.py to config.py after configuration."
+        "config.py not found. Rename config.example.py to config.py after configuration.",
+        file=stderr,
     )
     exit(1)
 
 
 def main():
-    if len(argv) != 2:
-        print(f"Usage: python3 {argv[0]} [on|off]", file=stderr)
+    if len(argv) != 3:
+        print(f"Usage: python3 {argv[0]} <day of week (num)> <on|off>", file=stderr)
         return 1
-    if argv[1] == "on":
+    try:
+        dow = int(argv[1])
+    except ValueError:
+        print(f"{argv[1]!r} is not a number", file=stderr)
+        return 1
+
+    # Monday is 1. Compatibility with crontab.
+    if dow % 7 != date.today().isoweekday() % 7:
+        return 0  # we just do nothing.
+
+    if argv[2] == "on":
         link_type = "any"
-    elif argv[1] == "off":
+    elif argv[2] == "off":
         link_type = "self"
     else:
         print(f"Unknown option {argv[1]!r}")
